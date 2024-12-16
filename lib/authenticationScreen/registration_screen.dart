d6156805550aa8329db640a6a8b6f73522cb0cab
pivnati <natanmuleta911@gmail.com>
2024-12-16 16:26:06 -0700

    make the registration file inside the registration file

--- lib/authenticationScreen/registration_screen.dart
+++ lib/authenticationScreen/registration_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tinder/widgets/custom_text_field_widget.dart';
 
 class RegistrationScreen extends StatefulWidget {
   const RegistrationScreen({super.key});
@@ -55,42 +56,77 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-       body: SingleChildScrollView(
+    return Scaffold(
+      body: SingleChildScrollView(
         child: Center(
           child: Column(
             children: [
-                const SizedBox(height: 20,),
-                const Text('Create Account',
+              const SizedBox(
+                height: 20,
+              ),
+              const Text(
+                'Create Account',
                 style: TextStyle(
-                  fontSize: 22,
-                  color: Colors.grey,
-                  fontWeight: FontWeight.bold
-                ),
-                ),
-                 const SizedBox(height: 22,),
-                 const Text(
-                  'to get started Now',
-                  style: TextStyle(
+                    fontSize: 22,
+                    color: Colors.grey,
+                    fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(
+                height: 22,
+              ),
+              const Text(
+                'to get started Now',
+                style: TextStyle(
                     fontSize: 18,
                     color: Colors.grey,
-                    fontWeight: FontWeight.bold                  ),
-                 ),
-                const SizedBox(height: 22,),
-                GestureDetector(
-                  onTap: () {
-                    
-                  },
-                  child:const CircleAvatar(
-                    radius: 80,
-                    backgroundImage: AssetImage('images/profile_avatar.jpg'),
-                  ),
+                    fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(
+                height: 22,
+              ),
+              //  upload the images
+              GestureDetector(
+                onTap: () {},
+                child: const CircleAvatar(
+                  radius: 80,
+                  backgroundImage: AssetImage('images/profile_avatar.jpg'),
+                ),
+              ),
 
-                )
+              const SizedBox(
+                height: 10,
+              ),
+
+              // Registration
+
+              SizedBox(
+                width: MediaQuery.of(context).size.width - 40,
+                height: 55,
+                child: CustomTextFieldWidget(
+                  editingController: emailTextEditingController,
+                  labelText: 'Email',
+                  iconData: Icons.email_outlined,
+                  isObscure: false,
+                ),
+              ),
+              const SizedBox(height: 20),
+              SizedBox(
+                width: MediaQuery.of(context).size.width - 40,
+                height: 55,
+                child: CustomTextFieldWidget(
+                  editingController: passwordTextEditingController,
+                  labelText: 'Password',
+                  iconData: Icons.lock_outline,
+                  isObscure: true,
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
             ],
           ),
         ),
-       ),
+      ),
     );
   }
 }
