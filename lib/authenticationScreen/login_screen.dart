6d3fd77654e3305fd2ceb5760a93b14b243aea16
pivnati <natanmuleta911@gmail.com>
2024-12-15 23:22:39 -0700

    add the log inside the login_screen

--- lib/authenticationScreen/login_screen.dart
+++ lib/authenticationScreen/login_screen.dart
@@ -9,12 +9,15 @@ import 'package:flutter/material.dart';
  class _LoginScreenState extends State<LoginScreen> {
    @override
    Widget build(BuildContext context) {
-     return const  Scaffold(
+     return Scaffold(
       body: SingleChildScrollView(
         child: Center(
             child: Column(
               children: [
-
+               Image.asset(
+                'images/tinder_logo.png',
+                width:200,
+               )
               ],
             ),
         ),
