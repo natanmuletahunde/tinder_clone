5b2d191863bf7b3f106e75a148cca5b52b789350
pivnati <natanmuleta911@gmail.com>
2024-12-16 16:04:49 -0700

    intailize the different types of controller

--- lib/authenticationScreen/registration_screen.dart
+++ lib/authenticationScreen/registration_screen.dart
@@ -8,15 +8,51 @@ class RegistrationScreen extends StatefulWidget {
 }
 
 class _RegistrationScreenState extends State<RegistrationScreen> {
-    //  Personal info 
-    TextEditingController emailTextEditingController = TextEditingController();
-    TextEditingController passwordTextEditingController = TextEditingController();
-    TextEditingController nameTextEditingController = TextEditingController();
-    TextEditingController ageTextEditingController = TextEditingController();
-    TextEditingController phoneNoTextEditingController = TextEditingController();
-    TextEditingController cityTextEditingController = TextEditingController();
-    TextEditingController countryTextEditingController = TextEditingController();
-    TextEditingController profileHeadingTextEditingController = TextEditingController();
+  //  Personal info
+  TextEditingController emailTextEditingController = TextEditingController();
+  TextEditingController passwordTextEditingController = TextEditingController();
+  TextEditingController nameTextEditingController = TextEditingController();
+  TextEditingController ageTextEditingController = TextEditingController();
+  TextEditingController phoneNoTextEditingController = TextEditingController();
+  TextEditingController cityTextEditingController = TextEditingController();
+  TextEditingController countryTextEditingController = TextEditingController();
+  TextEditingController profileHeadingTextEditingController =
+      TextEditingController();
+  TextEditingController lookingForInapartnerTextEditingController =
+      TextEditingController();
+  // Appearance
+  TextEditingController heightTextEditingController = TextEditingController();
+  TextEditingController weightTextEditingController = TextEditingController();
+  TextEditingController bodyTypeTextEditingController = TextEditingController();
+  // Life style
+  TextEditingController drinkTextEditingController = TextEditingController();
+  TextEditingController smokeTextEditingController = TextEditingController();
+  TextEditingController martialStatusTextEditingController =
+      TextEditingController();
+  TextEditingController haveChildrenTextEditingController =
+      TextEditingController();
+  TextEditingController noOfChildrenTextEditingController =
+      TextEditingController();
+  TextEditingController employmentStatusTextEditingController =
+      TextEditingController();
+  TextEditingController incomeTextEditingController = TextEditingController();
+  TextEditingController livingSituationTextEditingController =
+      TextEditingController();
+  TextEditingController willingToRelocateTextEditingController =
+      TextEditingController();
+  TextEditingController relationshipYouAreLookingForTextEditingController =
+      TextEditingController();
+  // Background  - Cultural Values
+  TextEditingController nationalityTextEditingController =
+      TextEditingController();
+  TextEditingController educationTextEditingController =
+      TextEditingController();
+  TextEditingController languageSpokenTextEditingController =
+      TextEditingController();
+  TextEditingController religionTextEditingController = TextEditingController();
+  TextEditingController ethnicityTextEditingController =
+      TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
