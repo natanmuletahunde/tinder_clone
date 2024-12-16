a94232b4fb91401a71ecdd219cabc28aec342d26
pivnati <natanmuleta911@gmail.com>
2024-12-16 15:47:43 -0700

    add the personal info on it

--- lib/authenticationScreen/registration_screen.dart
+++ lib/authenticationScreen/registration_screen.dart
@@ -8,8 +8,17 @@ class RegistrationScreen extends StatefulWidget {
 }
 
 class _RegistrationScreenState extends State<RegistrationScreen> {
+    //  Personal info 
+    TextEditingController emailTextEditingController = TextEditingController();
+    TextEditingController passwordTextEditingController = TextEditingController();
+    TextEditingController nameTextEditingController = TextEditingController();
+    TextEditingController ageTextEditingController = TextEditingController();
+    TextEditingController phoneNoTextEditingController = TextEditingController();
+    TextEditingController cityTextEditingController = TextEditingController();
+    TextEditingController countryTextEditingController = TextEditingController();
+    TextEditingController profileHeadingTextEditingController = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return const Placeholder();
   }
 }
