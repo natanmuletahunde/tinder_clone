f74edc8f11c7e53066a56d7198cb05950e15b787
pivnati <natanmuleta911@gmail.com>
2024-12-17 16:37:24 -0700

    modify the authentication_contoler

--- lib/controllers/authentication_controller.dart
+++ lib/controllers/authentication_controller.dart
@@ -1,4 +1,19 @@
+import 'dart:io';
+
 import 'package:get/get.dart';
+import 'package:image_picker/image_picker.dart';
 class AuthenticationController {
    static AuthenticationController authenticationController = Get.find();
+   late Rx<File?> pickedFile;
+   File? get profileImage => pickedFile.value;
+
+   pickImageFileFromGallery() async {
+    final imageFile = await ImagePicker().pickImage(source:ImageSource.gallery);
+    if(imageFile ! =null)
+    {
+      Get.snackbar('Profile Image', 'you have successfully picked your profile image.');
+    }
+
+    pickedFile = Rx<File?>(File(imageFile!.path));
+   }
 }
