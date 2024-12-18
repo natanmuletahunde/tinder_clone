65fa48949d1bec751dfe8c7f51e33ec21b9e0a1c
pivnati <natanmuleta911@gmail.com>
2024-12-18 12:41:50 -0700

    initialized the firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:firebase_core/firebase_core.dart'; // Import Firebase Core package
 import 'package:tinder/authenticationScreen/login_screen.dart';
 import 'package:tinder/controllers/authentication_controller.dart';
+import 'firebase_options.dart'; // Import Firebase options file
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  
-  // Initialize AuthenticationController
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   Get.put(AuthenticationController());
-  
   runApp(const MyApp());
 }
 
