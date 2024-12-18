e04a43ffc758d3a6f720fdc8a94cb41ae1216d49
pivnati <natanmuleta911@gmail.com>
2024-12-18 14:38:11 -0700

    cmake the auhtnticaiton to have the errormessage and json file

--- lib/controllers/authentication_controller.dart
+++ lib/controllers/authentication_controller.dart
@@ -3,7 +3,6 @@ import 'package:get/get.dart';
 import 'package:image_picker/image_picker.dart';
 
 class AuthenticationController extends GetxController {
-
   static AuthenticationController get instance => Get.find();
 
   // Observable for storing the picked file
@@ -15,10 +14,12 @@ class AuthenticationController extends GetxController {
 
   pickImageFileFromGallery() async {
     try {
-       XFile? imageFile = await ImagePicker().pickImage(source: ImageSource.gallery);
+      XFile? imageFile =
+          await ImagePicker().pickImage(source: ImageSource.gallery);
       if (imageFile != null) {
         pickedFile.value = File(imageFile.path);
-        Get.snackbar('Profile Image', 'You have successfully picked your profile image.');
+        Get.snackbar('Profile Image',
+            'You have successfully picked your profile image.');
       }
     } catch (e) {
       Get.snackbar('Error', 'Failed to pick image: $e');
@@ -26,15 +27,58 @@ class AuthenticationController extends GetxController {
   }
 
   // Method to capture an image from the camera
+
   captureImageFromPhoneCamera() async {
     try {
-       XFile? imageFile = await ImagePicker().pickImage(source: ImageSource.camera);
+      XFile? imageFile =
+          await ImagePicker().pickImage(source: ImageSource.camera);
       if (imageFile != null) {
         pickedFile.value = File(imageFile.path);
-        Get.snackbar('Profile Image', 'You have successfully captured your profile image using the camera.');
+        Get.snackbar('Profile Image',
+            'You have successfully captured your profile image using the camera.');
       }
     } catch (e) {
       Get.snackbar('Error', 'Failed to capture image: $e');
     }
   }
+
+  createNewUserAccount(
+    String imageProfile,
+    String name,
+    String age,
+    String phoneNo,
+    String city,
+    String country,
+    String profileHeading,
+    String lookingForInAPartner,
+    String height,
+    String bodyType,
+    String width,
+    String drink,
+    String smoke,
+    String maritalStatus,
+    String haveChildren,
+    String noOfChildren,
+    String profession,
+    String employmentStatus,
+    String income,
+    String livingSituation,
+    String willingToRelocate,
+    String relationshipYouAreLookingFor,
+    String nationality,
+    String education,
+    String languageSpoken,
+    String religion,
+    String ethnicity,
+    int publishedDateTime,
+  ) 
+  {
+    try{
+
+    } 
+    catch(errorMsg){
+     Get.snackbar('Account Creation Unsuccessful', 'Error occured: $errorMsg');
+    }
+
+  } 
 }
