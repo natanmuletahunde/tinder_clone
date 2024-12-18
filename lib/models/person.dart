d36a58ee2c79beb34537e42d193d5d95e0597c63
pivnati <natanmuleta911@gmail.com>
2024-12-18 13:02:10 -0700

    modify the code of the person.dart

---
+++ lib/models/person.dart
@@ -0,0 +1,33 @@
+class Person {
+  // personal information
+  String?name;
+  String?age;
+  String?phoneNo;
+  String?city;
+  String?country;
+  String?profileHeading;
+  String?lookingforinapartner;
+  // appearance 
+  String?height;
+  String?bodyType;
+  String?width;
+  // Life style 
+  String?drink;
+  String?smoke;
+  String?martialStatus;
+  String?haveChildren;
+  String?noOfChildren;
+  String?profession;
+  String?employmentStatus;
+  String?income;
+  String?livingSituation;
+  String?willingToRelocate;
+  String?relationshipYouAreLookingFor;
+  // BackGround-cultural values
+  String?nationality;
+  String?education;
+  String?languageSpoken;
+  String?religion;
+  String?ethnicity;
+
+}
