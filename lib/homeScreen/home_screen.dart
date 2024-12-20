16f8785aa2c64690132db6b79ac6f5cc88c4fe8b
pivnati <natanmuleta911@gmail.com>
2024-12-19 22:07:17 -0700

    create the homescreen page and in the homescreen folder

---
+++ lib/homeScreen/home_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+
+      body: Center(
+        child:   Text('Welcome ',
+        style: TextStyle(
+          fontWeight: FontWeight.bold,
+          fontSize: 20,
+          color:Colors.black
+        ),),
+      ),
+    );
+  }
+}
