5a7291a507507413194bc3a34c4c8e4ac1d32f9a
pivnati <natanmuleta911@gmail.com>
2024-12-20 19:57:30 -0700

    create the like_  modfiy it

--- lib/tabScreen/like_sent_like_received_screen.dart
+++ lib/tabScreen/like_sent_like_received_screen.dart
@@ -1,2 +1,25 @@
 import 'package:flutter/material.dart';
 
+class LikeSentLikeReceivedScreen extends StatefulWidget {
+  const LikeSentLikeReceivedScreen({super.key});
+
+  @override
+  State<LikeSentLikeReceivedScreen> createState() => _LikeSentLikeReceivedScreenState();
+}
+
+class _LikeSentLikeReceivedScreenState extends State<LikeSentLikeReceivedScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const  Scaffold(
+     body: Center(
+      child: Text(
+        'Like Sent/Like Received Screen',
+        style: TextStyle(fontSize: 24,
+        
+        fontWeight: FontWeight.bold,
+        color: Colors.white),
+      ),
+     ),
+    );
+  }
+}
