09dbd1c178407cef0aee04f4f0f7d9919316cdde
pivnati <natanmuleta911@gmail.com>
2024-12-20 20:06:10 -0700

    add all the tabscreen in the home screen

--- lib/homeScreen/home_screen.dart
+++ lib/homeScreen/home_screen.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:tinder/tabScreen/favorite_sent_favorite_received_screen.dart';
+import 'package:tinder/tabScreen/like_sent_like_received_screen.dart';
+import 'package:tinder/tabScreen/swipping_screen.dart';
+import 'package:tinder/tabScreen/user_details_screen.dart';
+import 'package:tinder/tabScreen/view_sent_view_received_screen.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -8,6 +13,18 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+   
+   int screenIndex = 0;
+   List  tabScreenList =  [
+    SwippingScreen(),
+    ViewSentViewReceivedScreen(),
+    FavoriteSentFavoriteReceivedScreen(),
+    LikeSentLikeReceivedScreen(),
+    UserDetailsScreen()
+   ];
+
+
+
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
